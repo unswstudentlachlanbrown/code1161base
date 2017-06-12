675f2a4312dbeac9f0744daea047c500ed723960
lachlan <lachlan.benjamin.brown@gmail.com>
2017-06-12 10:13:04 +1000

    completed all functions, passed linter

--- week3/exercise1.py
+++ week3/exercise1.py
@@ -12,16 +12,11 @@ def loop_ranger(start, stop=None, step=1):
 
     Do this using any method apart from just using range()
     """
-
-
-new_list = []
-for i in range(start, stop, step):
-    new_list.append(i)
+    new_list = []
+    while start < stop:
+        new_list.append(start)
+        start += step
     return new_list
-
-    print('this will error')
-
-    return "hello"
 
 
 def lone_ranger(start, stop, step):
@@ -47,18 +42,28 @@ def gene_krupa_range(start, stop, even_step, odd_step):
     make a list that instead of having evenly spaced steps
     has odd steps be one size and even steps be another.
     """
-    gene_list = []
-    latest = start
-    count = 0
-    while latest < stop:
-        gene_list.append(latest)
-        if beat % 2 == 0:
-            latest += even_step
+    range_list = []
+    counter = 1
+    while start <= stop:
+        range_list.append(start)
+        if counter % 2 == 1:
+            start += even_step
         else:
-            latest = odd_step
-        beat = beat + 1
-    return gene_list
+            start += odd_step
+        counter += 1
+    return range_list
 
+    # gene_list = []
+    # latest = start
+    # count = 0
+    # while latest < stop:
+    #     gene_list.append(latest)
+    #     if beat % 2 == 0:
+    #         latest += even_step
+    #     else:
+    #         latest = odd_step
+    #     beat = beat + 1
+    # return gene_list
 
     """Ask for a number, and if the response is outside the bounds keep asking
     until you get a number that you think is OK
@@ -73,21 +78,19 @@ def stubborn_asker(low, high):
     """
     message = "number between {low} and {high}:".format(low=low, high=high)
 
-while True:
-    input_number = int(raw_input(message))
-    if low < input_number < high:
-        print("good {} looks good!".format(input_number))
-        return input_number
-    else:
-        print("{input} isn't between {low}, and {high}".format(
-                                        input=input_number, low=low, high=high))
+    while True:
+        input_number = int(raw_input(message))
+        if low < input_number < high:
+            print("good {} looks good!".format(input_number))
+            return input_number
+        else:
+            print("{input} isn't between {low}, and {high}".format(
+                                    input=input_number, low=low, high=high))
+        print(low < input_number, input_number < high)
 
+        input_number = int(raw_input(message))
+        print(low < input_number < high)
 
-    """print(low < input_number, input_number < high)
-
-    input_number = int(raw_input(message))
-    print(low < input_number < high)
-    """
 
 def not_number_rejector(message):
     """Ask for a number repeatedly until actually given one.
@@ -100,7 +103,7 @@ def not_number_rejector(message):
 
     while True:
         try:
-            input_number = int(raw_input(messge))
+            input_number = int(raw_input(message))
             print("thanks! {} looks good".format(input_number))
             return input_number
         except Exception as e:
@@ -113,17 +116,36 @@ def super_asker(low, high):
     Combine stubborn_asker and not_number_rejector to make a function
     that does it all!
     """
-
-message = "give me a number between {} and {}:".format(low, high)
-
+    message = ("Type a number between {} and {}:".format(low, high))
     while True:
-        try:
-            i_no = int(raw_input(message))
-            if low < i_no < high:
-                print("correct {}").format(i_no))
-                return i_no
-            else:
-                print("{}incorrect, not between {} and {}")
+            while True:
+                try:
+                    input_number = int(raw_input(message))
+                    print("{}is a number".format(input_number))
+                    if low < input_number < high:
+                        print("{} is vaild".format(input_number))
+                        return(input_number)
+                    else:
+                        print("{} is not vaild".format(input_number))
+                except Exception as e:
+                    print("Try again ({})".format(e))
+
+    return(input_number)
+    # message = "give me a number between {} and {}:".format(low, high)
+    #
+    # answered = False
+    # while answered is False:
+    #     answered = True
+    #     num = raw_input("type number between {} and {}".format(low, high))
+    #
+    # while True:
+    #         try:
+    #             i_no = int(raw_input(message))
+    #             if low < i_no < high:
+    #                 print("correct {}").format(i_no)
+    #                 return i_no
+    #             else:
+    #                 print("{}incorrect, not between {} and {}")
 
 
 if __name__ == "__main__":

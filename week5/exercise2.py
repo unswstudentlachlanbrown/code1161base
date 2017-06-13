ddbd6eff96a2137757c20e8d915893eb970625ae
lachlan <lachlan.benjamin.brown@gmail.com>
2017-06-13 23:52:42 +1000

    finished

--- week5/exercise2.py
+++ week5/exercise2.py
@@ -97,16 +97,23 @@ def abba(source="abba", guard=3):
         You need to change these substitutions to make it work.
         """
         if letter == "a":
-            return "a"
+            return "bba"
         elif letter == "b":
-            return "b"
+            return "aob"
         elif letter == "o":
-            return "o"
+            return "oa"
         else:
             return letter
-
-    # write the rest of the function here
-    pass
+            
+    parts = list(source)
+    result = map(apply_rules, parts)
+    abba_str = "".join(result)
+    print(abba_str)
+    guard == 1
+    if guard > 0:
+        return abba(abba_str, guard)
+    else:
+        return abba_str
 
 
 def koch(t, order, size):
@@ -150,9 +157,13 @@ def square_koch(t, order, size):
 
     """
     trace = ""
-    # write the rest of the function here.
+    if order == 0:
+        t.forward(size)
+    else:
+        for angle in [90, -90, 90, 0]:
+            trace += square_koch(t, order-1, size/3)
+            t.left(angle)
     return str(order) + trace
-    pass
 
 
 def draw_square(steps=4):
